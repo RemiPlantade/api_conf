7211444066a9e564f05b4074a37cde8569571bb8
remi_ <d0r3mu5@gmail.com>
2018-08-31 15:38:59 +0200

    Add BadURLException Class

---
+++ src/main/java/api_conf/conf/exception/BadURLException.java
@@ -0,0 +1,16 @@
+package api_conf.conf.exception;
+
+public class BadURLException extends Exception {
+
+	public BadURLException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadURLException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+		
+}
